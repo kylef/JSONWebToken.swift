65ab050c7bc1073a6d634d3105a575efb5edc70a
Kyle Fuller <kyle@fuller.li>
2016-09-13 10:29:08 +0100

    Update CryptoSwift to 0.6.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "JWT",
   dependencies: [
-    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift", versions: Version(0,2,2)..<Version(0,3,0))
+    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift", versions: Version(0, 6, 0) ..< Version(0, 7, 0))
   ]
 )
