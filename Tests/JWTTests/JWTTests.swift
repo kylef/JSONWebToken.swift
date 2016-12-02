7e0c9a4c433993f6cda5288d205b252e352998bd
Kyle Fuller <kyle@fuller.li>
2016-12-02 19:31:57 +0000

    fix(tests): Account for differences in JSON serialization
    This fixes testing on iOS 8 and Linux.
    
    Closes #22

--- Tests/JWTTests/JWTTests.swift
+++ Tests/JWTTests/JWTTests.swift
@@ -6,8 +6,16 @@ class EncodeTests: XCTestCase {
   func testEncodingJWT() {
     let payload = ["name": "Kyle"] as Payload
     let jwt = JWT.encode(payload, algorithm: .hs256("secret".data(using: .utf8)!))
-    let fixture = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiS3lsZSJ9.zxm7xcp1eZtZhp4t-nlw09ATQnnFKIiSN83uG8u6cAg"
-    XCTAssertEqual(jwt, fixture)
+
+    let expected = [
+      // { "alg": "HS256", "typ": "JWT" }
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiS3lsZSJ9.zxm7xcp1eZtZhp4t-nlw09ATQnnFKIiSN83uG8u6cAg",
+
+      // {  "typ": "JWT", "alg": "HS256" }
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiS3lsZSJ9.4tCpoxfyfjbUyLjm9_zu-r52Vxn6bFq9kp6Rt9xMs4A",
+    ]
+
+    XCTAssertTrue(expected.contains(jwt))
   }
 
   func testEncodingWithBuilder() {
