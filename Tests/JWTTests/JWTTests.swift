78dc2010cf00a7d3c9bfcddacd554bf6096697aa
Jan Brinker <jb@janbrinker.de>
2017-09-07 16:40:34 +0200

    Added some integration tests for verification of dates with  and without leeway.

--- Tests/JWTTests/JWTTests.swift
+++ Tests/JWTTests/JWTTests.swift
@@ -362,6 +362,46 @@ class ValidationTests: XCTestCase {
   }
 }
 
+class IntegrationTests: XCTestCase {
+  func testVerificationFailureWithoutLeeway() {
+    let token = JWT.encode(.none) { builder in
+      builder.issuer = "fuller.li"
+      builder.audience = "cocoapods"
+      builder.expiration = Date().addingTimeInterval(-1) // Token expired one second ago
+      builder.notBefore = Date().addingTimeInterval(1) // Token starts being valid in one second
+      builder.issuedAt = Date().addingTimeInterval(1) // Token is issued one second in the future
+    }
+    
+    let expectation = XCTestExpectation(description: "Verification should fail.")
+    do {
+      let _ = try JWT.decode(token, algorithm: .none)
+      XCTFail("InvalidToken error should have been thrown.")
+    } catch is InvalidToken {
+      expectation.fulfill()
+    } catch {
+      XCTFail("Unexpected error type while verifying token.")
+    }
+    self.wait(for: [expectation], timeout: 0.5)
+  }
+  
+  func testVerificationSuccessWithLeeway() {
+    let token = JWT.encode(.none) { builder in
+      builder.issuer = "fuller.li"
+      builder.audience = "cocoapods"
+      builder.expiration = Date().addingTimeInterval(-1) // Token expired one second ago
+      builder.notBefore = Date().addingTimeInterval(1) // Token starts being valid in one second
+      builder.issuedAt = Date().addingTimeInterval(1) // Token is issued one second in the future
+    }
+    
+    do {
+      let _ = try JWT.decode(token, algorithm: .none, leeway: 2)
+      // Due to leeway no error gets thrown.
+    } catch {
+      XCTFail("Unexpected error type while verifying token.")
+    }
+  }
+}
+
 // MARK: Helpers
 
 func assertSuccess(_ decoder: @autoclosure () throws -> Payload, closure: ((Payload) -> Void)? = nil) {
