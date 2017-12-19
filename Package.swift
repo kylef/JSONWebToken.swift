fb3584dc3d4b716379cede2609c970b804b8205c
Jonathan Thornton <jonblatho@icloud.com>
2017-12-19 00:25:42 -0600

    fix spacing issue in Package.swift
    Signed-off-by: Jonathan Thornton <jonblatho@icloud.com>

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 let package = Package(
-   name: "JWT",
-   dependencies: [
-     .Package(url: "https://github.com/kylef-archive/CommonCrypto.git", majorVersion: 1),
+  name: "JWT",
+  dependencies: [
+    .Package(url: "https://github.com/kylef-archive/CommonCrypto.git", majorVersion: 1),
   ],
   exclude: [
     "Sources/JWT/HMACCryptoSwift.swift",
