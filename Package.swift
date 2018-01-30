39e84fe9bad073e02746112bc45152c3585c0ea4
danielpater <danielpater@users.noreply.github.com>
2018-01-30 21:03:09 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,7 @@
 import PackageDescription
 
 
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let package = Package(
-  name: "JWT",
-  dependencies: [
-    .Package(url: "https://github.com/kylef-archive/CommonCrypto.git", majorVersion: 1),
-  ],
-  exclude: [
-    "Sources/JWT/HMACCryptoSwift.swift",
-  ]
-)
-#else
+
 let package = Package(
   name: "JWT",
   dependencies: [
@@ -21,4 +11,4 @@ let package = Package(
     "Sources/JWT/HMACCommonCrypto.swift",
   ]
 )
-#endif
+
