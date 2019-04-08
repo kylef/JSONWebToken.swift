f8ef984eb75e243adf2df14d147e3b6a4e37a53a
edward <cuikefan@bituniverse.org>
2019-04-08 20:19:56 +0800

    fit on Xcode10.2

--- Package.swift
+++ Package.swift
@@ -1,22 +1,6 @@
 // swift-tools-version:4.0
 
-import PackageDescription
-
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let dependencies = [
-  Package.Dependency.package(url: "https://github.com/kylef-archive/CommonCrypto.git", from: "1.0.0"),
-]
-let excludes = ["HMAC/HMACCryptoSwift.swift"]
-let targetDependencies: [Target.Dependency] = []
-#else
-let dependencies = [
-  Package.Dependency.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
-]
-let excludes = ["HMAC/HMACCommonCrypto.swift"]
-let targetDependencies: [Target.Dependency] = ["CryptoSwift"]
-#endif
-
+import PackageDescriptio
 
 let package = Package(
   name: "JWT",
