c6c8501f6711e3139da5ad8a55077f256465905a
edward <cuikefan@bituniverse.org>
2019-04-08 20:07:01 +0800

    Fit on swift5

--- Package.swift
+++ Package.swift
@@ -2,22 +2,6 @@
 
 import PackageDescription
 
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let dependencies = [
-  Package.Dependency.package(url: "https://github.com/kylef-archive/CommonCrypto.git", from: "1.0.0"),
-]
-let excludes = ["HMAC/HMACCryptoSwift.swift"]
-let targetDependencies: [Target.Dependency] = []
-#else
-let dependencies = [
-  Package.Dependency.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
-]
-let excludes = ["HMAC/HMACCommonCrypto.swift"]
-let targetDependencies: [Target.Dependency] = ["CryptoSwift"]
-#endif
-
-
 let package = Package(
   name: "JWT",
   products: [
